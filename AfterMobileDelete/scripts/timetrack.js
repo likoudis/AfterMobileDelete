e3fa3aa257963266672498e48c90804b4f2e583d
Paul Likoudis <likoudis@hotmail.com>
2014-02-17 14:47:38 -0600

    Removed DataSource replaced with array Removed all alerts...

--- AfterMobileDelete/scripts/timetrack.js
+++ AfterMobileDelete/scripts/timetrack.js
@@ -2,44 +2,48 @@
     app = global.app = global.app || {};
     var TimetrackModel;
     var starts = new Array();
-    var stops = new Array();
+    var stops  = new Array();
     
     TimetrackModel = kendo.data.ObservableObject.extend({
         
         isTimetrackModelInitialized: false,
         timetrackDataSource : null,
-        
+        timetrackData: [],
+
         init: function() {
             var that = this,
             dataSource;
           
-            alert("in model init 1");
             kendo.data.ObservableObject.fn.init.apply(that, []);
-            alert("in model init 2");
             
-           dataSource = new kendo.data.DataSource({
+            dataSource = new kendo.data.DataSource({
                 data: [ 
                     {start: starts[0], stop: "stop 1"},
                     {start: "start 2", stop: "stop 2"}
                 ]
             });
             
-            alert(dataSource);
             that.set("timetrackDataSource", dataSource);
-            alert("after setting datasource");
+            that.set("timeTrackData", []);
         },
         
         onStart: function(e) {
-            alert("in onstart");
-            starts[starts.length] = Date();
-            alert("starts " + starts.length);
-            alert(this.timetrackDataSource);
+            var temp = [];
+            var that = this;
+            
+            temp = that.get("timeTrackData");
+            temp.push({start: Date(), stop: ""});
+            that.set("timeTrackData", temp);
         },
         
         onStop: function(e) {
-            alert("in onstop");
-            stops[stops.length] = Date();
-            alert("stop " + stops.length);
+            var temp = [];
+            var that = this;
+            
+            temp = that.get("timeTrackData");
+            //temp[temp.length - 1].start = "shit";
+            temp.push({start: "", stop: Date()});
+            that.set("timeTrackData", temp);
         }
     });
     
@@ -48,18 +52,13 @@
             starts = new Array();
             stops = new Array();
             app.timetrackService.viewModel.set("isTimeTrackModelInitialized", true);            
-            alert("in initTimeTrack");
         },
         
         click: function(e) {
             if (e.sender.element.text() === "Start") {
-                alert("in start before onstart" + " " + e.sender.element.text());
                 app.timetrackService.viewModel.onStart(e);
-                alert("back from onstart");
                 e.sender.element.text("Stop");
-
             } else {
-                alert("in stop before onstop");
                 app.timetrackService.viewModel.onStop(e);
                 e.sender.element.text("Start");
             }
